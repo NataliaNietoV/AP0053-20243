3bb6cbc0a2b7815a77f4d7d26dad17e372b969e9
NataliaNietoV <nnietov@udistrital.edu.co>
2024-09-17 09:59:04 -0500

    Add files via upload

---
+++ Corte 1/main.py
@@ -0,0 +1,15 @@
+import clase
+# creating object for Square class
+
+
+objecto_1 = clase.Potencia(5)
+objecto_1.getVal()
+
+
+object2 = clase.Opeacion()
+
+object2.suma(2,4)
+object2.resta(2,3)
+object2.multiplicacion(2,3)
+object2.division(2,3)
+
